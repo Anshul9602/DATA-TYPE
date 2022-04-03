47fb2980be5a1b9524db6051bf1627c7efaa1457
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-04-03 13:41:18 +0530

    Add files via upload

---
+++ DATATYPE.java
@@ -0,0 +1,11 @@
+
+public class DATATYPE{
+    public  static void main(String[] args){
+        int a =657+45;
+        float b = a+ 4.5f;
+        float c =65.7f+4.5f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
